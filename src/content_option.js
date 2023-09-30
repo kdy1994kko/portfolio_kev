db45a1d9cf4759c3b71ed623aa975be59ceeab7f
Kevvan D. Young <101077732+kdy1994kko@users.noreply.github.com>
2023-09-30 17:52:21 -0600

    Add files via upload

--- src/content_option.js
+++ src/content_option.js
@@ -7,9 +7,9 @@ const meta = {
 const introdata = {
     title: "I’m Kevvan D. Young",
     animated: {
-        first: "Retired US Airforce Veteran",
-        second: "I love coding",
-        third: "I code cool websites",
+        first: "Jesus Loves You",
+        second: "You Are Forgiven",
+        third: "Accept Him As Your Lord And Savior",
     },
     description: "Holy Faith Developer",
     your_img_url: "https://scontent-dfw5-2.xx.fbcdn.net/v/t39.30808-6/375767800_6790505337662058_7538141220056060371_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=a2f6c7&_nc_ohc=XxIrCZa2mj4AX9hOi6R&_nc_ht=scontent-dfw5-2.xx&oh=00_AfDmGiw82pgtuFrcg1lt_7RRtRPTf5hfiGuyJTGoSk1t7Q&oe=651DD627",
@@ -93,9 +93,9 @@ const dataportfolio = [{
         link: "#",
     },
     {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://1.bp.blogspot.com/-aYASivipi0w/WEj0h8GkkWI/AAAAAAAAC0E/xLe7hllrrf0Ud35uBa74yD_N5-b_yhQZQCEw/s1600/shutterstock-242288239_orig.jpg",
+        description: "My First Portfolio",
+        link: "https://codepen.io/kdy1994kko/full/YzdKXyN",
     },
     {
         img: "https://picsum.photos/400/700/?grayscale",
@@ -138,7 +138,7 @@ const dataportfolio = [{
 const contactConfig = {
     YOUR_EMAIL: "kdy1994kko@gmail.com",
     YOUR_FONE: "281-896-2303",
-    description: "Loem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    description: "'There will be no more death, mourning, crying or pain' ~ Revelation 21:4",
     // creat an emailjs.com account 
     // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
     YOUR_SERVICE_ID: "service_id",
